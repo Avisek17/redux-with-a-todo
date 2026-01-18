5f01b1e5849494fe21421618c3d401d43994d6d2
Avisek <[avisekdulal07@gmail.com]>
2026-01-18 16:59:09 +0545

    todo redux

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import AddTodo from './components/AddTodo'
 import Todos from './components/Todos'
 
 function App() {
-  
+ 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <AddTodo />
-    <Todos />
+    <h1 className='text-3xl'>Todo Redux</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
